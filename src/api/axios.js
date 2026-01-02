e7569ad8efc955c599e4deb9e5ab5bd9debbd894
Ezeagu Kosi <kosialex66@gmail.com>
2026-01-02 07:36:27 +0100

    base url updated

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create ({
-    baseURL: 'http://localhost:8000/',
+    baseURL: 'https://juike-exams-token-sale-web-app-django.onrender.com/',
 });
 
 export default instance;
