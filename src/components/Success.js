6cb1631a5b7e5f6503562da55ef488e2875aaca7
Ezeagu Kosi <kosialex66@gmail.com>
2026-01-07 04:33:56 +0100

    url update

--- src/components/Success.js
+++ src/components/Success.js
@@ -20,7 +20,7 @@ export default function PurchaseSuccess() {
     if (!reference) return;
 
     axios
-      .get(`https://juike-exams-token-sale-web-app-django.onrender.com/api/verify-paystack/`)
+      .get(`https://juike-exams-token-sale-web-app-django.onrender.com/api/verify-paystack/?reference=${reference}`)
       .then((res) => {
         console.log("Payment verified");
       })
@@ -35,7 +35,7 @@ export default function PurchaseSuccess() {
 
     const interval = setInterval(() => {
       axios
-        .get(`https://juike-exams-token-sale-web-app-django.onrender.com/api/assigned-tokens/`)
+        .get(`https://juike-exams-token-sale-web-app-django.onrender.com/api/assigned-tokens/?reference=${reference}`)
         .then((res) => {
           if (res.data.length > 0) {
             setAssignedTokens(res.data);
